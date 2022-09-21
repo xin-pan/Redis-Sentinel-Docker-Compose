33674fbf73c8d77d045dad21d55ec09c68b84b3a
panxi <panxi@tesla.com>
2022-09-21 10:29:16 +0800

    issue fix: ip address changed after wifi switched. solution: use machine name instead of ip

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-export MASTER_HOST=`ifconfig en0|grep "inet\ "| awk '{ print $2}'`
+#export MASTER_HOST=`ifconfig en0|grep "inet\ "| awk '{ print $2}'`
+export MASTER_HOST=`hostname`.local
+echo "MASTER_HOST=$MASTER_HOST"
 
 docker-compose down --rmi 'local'
 docker-compose up -d
